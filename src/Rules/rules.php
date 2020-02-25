afae1c7ce602e9c236d0af1ff97de4b1acaf588a
Sina <sharifzadesina@gmail.com>
2020-02-25 15:53:09 +0330

    Add support for signed numbers

--- src/Rules/rules.php
+++ src/Rules/rules.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Schema\Builder as Schema;
 Validator::extend('decimal', function ($attribute, $value, $parameters, $validator) {
     $integer = $parameters[0] - $parameters[1];
     $scale = $parameters[1];
-    return (bool) preg_match("/^\d{1,{$integer}}(\.\d{1,{$scale}})?$/", $value);
+    return (bool) preg_match("/^(\+|-)?\d{1,{$integer}}(\.\d{1,{$scale}})?$/", $value);
 });
 
 Validator::replacer('decimal', function ($message, $attribute, $rule, $parameters) {
