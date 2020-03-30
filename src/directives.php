fb93b7509565f9e4a806b6826fc4dfeccfc37674
Sina <sharifzadesina@gmail.com>
2020-03-31 03:37:41 +0430

    Support all local variables inside mixins

--- src/directives.php
+++ src/directives.php
@@ -6,7 +6,7 @@ Blade::directive('mixin', function ($exp) {
     return <<<CODE
 <?php
 list(\$_mixName) = (fn(\$name) => [\$name])({$exp});
-\$__data['_mixins'][\$_mixName.'-'.md5(__FILE__)] = function (\$_mix_data) use (&\$__data) { extract(array_replace(\$__data, \$_mix_data));
+\$_mixins[\$_mixName.'-'.md5(__FILE__)] = function (\$_mix_data) use (&\$_mixins) { extract(\$_mix_data);
 ?>
 CODE;
 });
@@ -23,7 +23,8 @@ Blade::directive('plus', function ($exp) {
     return <<<CODE
 <?php
 list(\$_mixName, \$_mixData) = (fn(\$name, array \$data = []) => [\$name, \$data])({$exp});
-\$__data['_mixins'][\$_mixName.'-'.md5(__FILE__)](\$_mixData);
+\$_mixData = array_replace(get_defined_vars(), \$_mixData);
+\$_mixins[\$_mixName.'-'.md5(__FILE__)](\$_mixData);
 ?>
 CODE;
 });
